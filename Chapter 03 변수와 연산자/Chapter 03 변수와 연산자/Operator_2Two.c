33b4fa94103110cad4c1c510eba089e488d748ae
Picik-picik <56762198+Picik-picik@users.noreply.github.com>
2019-10-20 22:09:42 +0900

    오타 수정 191020 일요일

--- Chapter 03 변수와 연산자/Chapter 03 변수와 연산자/Operator_2Two.c
+++ Chapter 03 변수와 연산자/Chapter 03 변수와 연산자/Operator_2Two.c
@@ -5,6 +5,6 @@ int main(void){
 	num1 += 3; // num1 = num1 + 3;
 	num2 *= 4; // num2 = num2 * 4;
 	num3 %= 5; // num3 = num3 % 5;
-	printf("Resuli : %d, %d, %d\n", num1, num2, num3);
+	printf("Result : %d, %d, %d\n", num1, num2, num3);
 	return 0;
 }
