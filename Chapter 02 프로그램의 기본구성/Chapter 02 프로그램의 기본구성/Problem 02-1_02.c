91a30719d63f38bf6db84266775ef47e10edb510
picik <alswnstn30@gmail.com>
2019-10-20 20:27:18 +0900

    윤성우의 열혈 C 프로그래밍 191006 일요일
    Chapter 02 프로그램의 기본구성

---
+++ Chapter 02 프로그램의 기본구성/Chapter 02 프로그램의 기본구성/Problem 02-1_02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	printf("巩力 02-1 [抗力 函版秦焊扁]\n");
+	printf("巩力 2)\n");
+	printf("捞    抚 : 全辨悼\n");
+	printf("林    家 : 全匡漂喊矫 全己悼 全己备 全全全\n");
+	printf("傈拳锅龋 : 010-1234-5678\n");
+	return 0;
+}
