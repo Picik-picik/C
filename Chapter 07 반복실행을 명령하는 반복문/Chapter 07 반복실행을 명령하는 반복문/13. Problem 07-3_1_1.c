61cf4296ec7e7bfad76e96d979b987ac66efc9a1
Picik-picik <56762198+Picik-picik@users.noreply.github.com>
2019-11-10 02:35:03 +0900

    내용 수정 191110 일요일

---
+++ Chapter 07 반복실행을 명령하는 반복문/Chapter 07 반복실행을 명령하는 반복문/13. Problem 07-3_1_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int num = 1, hap = 0;
+
+	printf("문제 07-3 [while문과 do~while문에 익숙해지기]\n");
+	printf("문제 1)\n");
+	printf("방법 1)\n");
+	while (num != 0) {
+		printf("더할 값을 입력하거나 시스템을 종료하려면 0을 입력하세요 : ");
+		scanf_s("%d", &num);
+		hap += num;
+	}
+
+	printf("총합 : %d\n", hap);
+	return 0;
+}
