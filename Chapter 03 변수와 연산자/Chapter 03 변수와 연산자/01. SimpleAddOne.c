67b4fb39091811d4698c73cc7e006497bf06990a
Picik-picik <56762198+Picik-picik@users.noreply.github.com>
2019-11-10 03:12:46 +0900

    파일 이름 수정 191110 일요일

---
+++ Chapter 03 변수와 연산자/Chapter 03 변수와 연산자/01. SimpleAddOne.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+	// 3 + 4; // 3과 4의 합을 명령함
+	int num;			// num이라는 이름의 변수선언
+	num = 20;			// 변수 num에 20 저장
+	printf("%d", num);	// 변수 num의 값 참조
+	
+	/*
+	int num; // num이라는 변수의 선언 
+	num = 12;	 // 변수 num을 12로 초기화 
+	num = 24;  // 변수 num에 24를 대입 
+
+	int num = 12; // 선언과 동시에 초기화 
+
+	int num1, num2;			// 두 개의 변수를 선언 
+	int num3 = 30, num4 = 40;	// 두 개의 변수를 선언 및 초기화 
+	*/
+	return 0;
+}
