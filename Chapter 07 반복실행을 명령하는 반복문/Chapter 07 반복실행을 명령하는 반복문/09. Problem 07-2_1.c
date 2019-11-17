e012f351d57352d183e89bcfbabbfc301fd418c7
Picik-picik <56762198+Picik-picik@users.noreply.github.com>
2019-11-17 20:10:42 +0900

    내용 수정 191117 일요일

--- Chapter 07 반복실행을 명령하는 반복문/Chapter 07 반복실행을 명령하는 반복문/09. Problem 07-2_1.c
+++ Chapter 07 반복실행을 명령하는 반복문/Chapter 07 반복실행을 명령하는 반복문/09. Problem 07-2_1.c
@@ -5,16 +5,17 @@ int main(void) {
 	
 	printf("문제 07-2 [while문의 중첩]\n");
 	printf("문제 1)\n");
-	printf("1 이상의 정수를 5개 입력해주세요 : \n");
+
 	while (i < 5) {
 		while (num <= 0) {
-			printf("%d번째\n", i+1);
+			printf("0보다 큰 수를 5개 입력하세요. %d번째\n", i+1);
 			scanf_s("%d", &num);
 		}
 		hap += num;
 		num = 0;
 		i++;
 	}
+	
 	printf("총합 : %d\n", hap);
 	return 0;
 }
