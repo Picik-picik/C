cc7a5b93574cc5d52cc12d764c6029ee0250680e
Picik-picik <56762198+Picik-picik@users.noreply.github.com>
2019-11-17 18:11:54 +0900

    내용 수정 191117 일요일

--- Chapter 04 데이터 표현방식의 이해/Chapter 04 데이터 표현방식의 이해/13. Problem 04-4_2.c
+++ Chapter 04 데이터 표현방식의 이해/Chapter 04 데이터 표현방식의 이해/13. Problem 04-4_2.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main(void){
-	int num1;
+	int num = 3;
 
 	printf("문제 04-4 [비트연산의 이해]\n");
 	printf("문제 2)\n");
 	// 연산자 사용하지 않고 3 X 8 / 4의 값을 출력
-	int num2 = 3;
-	num2 = num2 << 3;	// 8의 곱
-	num2 = num2 >> 2;	// 4의 제
-	printf("결과 : %d \n", num2);
+	num = num << 3;	// 8의 곱
+	num = num >> 2;	// 4의 제
+	printf("결과 : %d \n", num);
 	return 0;
 }
